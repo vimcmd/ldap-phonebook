1394d88523198f65dacbaa5c2328fedf55be203e
vim.cmd <vim.cmd@gmail.com>
2016-06-24 16:52:05 +0500

    display current user name

--- src/main/java/com/github/vimcmd/controller/HomeController.java
+++ src/main/java/com/github/vimcmd/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.github.vimcmd.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +12,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String providePhoneBook(Model model) {
-        // TODO: 24.06.2016 replace Object with phonebook domain object
-        model.addAttribute("users", new Object());
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        logger.warn("AUTHENTICATED: " + authentication.getPrincipal().toString());
+
+        model.addAttribute("currentUser", authentication.getName());
         return "phonebook";
     }
 
